013adcb890b738958d7ba2499a30fc2c53e0595d
wley3337@gmail.com <wley3337@gmail.com>
2018-05-07 00:53:10 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ results_array = []
+ while i < collection.length 
+ #take the value of each item and see if it is even (%2 == 0)
+    yield (if collection[i] % 2 == 0 
+    results_array.push(collection[i])
+    i += 1
+    else
+      i += 1 
+    end)
+  end
+  results_array
 end
