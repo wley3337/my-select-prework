22c233387eb907d7d30195b105bb5d3fc0ea1e6a
wley3337@gmail.com <wley3337@gmail.com>
2018-05-07 00:45:31 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,5 +3,10 @@ def my_select(collection)
  results_array = []
  while i < collection.length 
  #take the value of each item and see if it is even (%2 == 0)
-    
+    yield (if collection[i] % 2 == 0 
+    results_array.push(collection[i])
+    i += 1
+    else
+      i += 1 
+      end)
 end
